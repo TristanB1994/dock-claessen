dccdf6711c6d4bc0a69ca99fd887565c77fe3bed
TristanB1994 <t.blandford.dev@gmail.com>
2020-09-10 16:58:15 -0400

    more animation changes

--- flask/app/static/js/script.js
+++ flask/app/static/js/script.js
@@ -2,6 +2,8 @@ var logo = document.getElementById('logo');
 var logoTwo = document.getElementById('new_logo');
 var nav = document.getElementsByTagName('nav')[0];
 var foot = document.getElementsByClassName('footerlay')[0];
+var cpl = document.getElementsByClassName('CPL')[0];
+var cplnew = document.getElementById('cpl');
 
 $(document).ready(function() {
 
@@ -14,14 +16,15 @@ $(document).ready(function() {
     $(logo).one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", 
     function(event) {
         $(this).remove();
+        cpl.remove();
         newlogo();
     });
 
 });
 
 function newlogo(){
-    // logoTwo.classList.add('color');
     logoTwo.classList.add('color');
+    cplnew.classList.add('color');
     nav.classList.add('blueborder')
     foot.classList.add('Blue')
 }
