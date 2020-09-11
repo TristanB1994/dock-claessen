64891bff594627df161805ef2656412de113cd57
TristanB1994 <t.blandford.dev@gmail.com>
2020-09-11 14:39:41 -0400

    updated styles

--- flask/app/static/js/script.js
+++ flask/app/static/js/script.js
@@ -4,6 +4,11 @@ var nav = document.getElementsByTagName('nav')[0];
 var foot = document.getElementsByClassName('footerlay')[0];
 var cpl = document.getElementsByClassName('CPL')[0];
 var cplnew = document.getElementById('cpl');
+// banner slides
+var banner = document.getElementsByClassName('homebanner')[0];
+var slide1 = banner.getElementsByClassName('scroll-banner-1');
+var slide2 = banner.getElementsByClassName('scroll-banner-2');
+var slide3 = banner.getElementsByClassName('scroll-banner-3');
 
 $(document).ready(function() {
 
@@ -15,8 +20,11 @@ $(document).ready(function() {
     $(logo).one('animationend', function(event) {
         $(this).remove();
         cpl.remove();
-        newlogo();
+        newlogo()
     })
+    $(cplnew).one('animationend', function(event) {
+        bannerMsg();
+    });
 
 });
 
@@ -25,4 +33,13 @@ function newlogo(){
     cplnew.classList.add('color');
     nav.classList.add('blueborder')
     foot.classList.add('Blue')
+}
+
+// Banner slides
+
+function bannerMsg() {
+    $(slide1).hide();
+    $(slide2).show(50, function(){
+        console.log('slide2 ran');
+    });
 }
